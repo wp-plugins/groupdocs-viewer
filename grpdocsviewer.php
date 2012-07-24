2b4a358c02a90000e6311b163627a2205f7bdc68
groupdocs <groupdocs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-24 14:49:02 +0000

    Final fix for backlash issue
    git-svn-id: https://plugins.svn.wordpress.org/groupdocs-viewer/trunk@576659 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- grpdocsviewer.php
+++ grpdocsviewer.php
@@ -6,7 +6,7 @@ Plugin URI: http://www.groupdocs.com/
 Description: Lets you embed PPT, PPTX, XLS, XLSX, DOC, DOCX, PDF and many other formats from your GroupDocs acount in a web page using the GroupDocs Embedded Viewer (no Flash or PDF browser plug-ins required).
 Author: GroupDocs Team <support@groupdocs.com>
 Author URI: http://www.groupdocs.com/
-Version: 1.2.1
+Version: 1.2.2
 License: GPLv2
 */
 
@@ -23,7 +23,7 @@ function grpdocs_getdocument($atts) {
 		'version' => 1,
 	), $atts));
 
-	
+
 	$guid = grpdocs_getGuid(urlencode($file));
 
 //	$code = "<iframe src='https://dev-apps.groupdocs.com/document-viewer/embed/{$guid}' frameborder='0' width='600' height='700'></iframe>";
@@ -69,5 +69,5 @@ function grpdocs_options() {
 	if ( function_exists('current_user_can') && !current_user_can('manage_options') ) die(t('An error occurred.'));
 	if (! user_can_access_admin_page()) wp_die('You do not have sufficient permissions to access this page');
 
-	require(ABSPATH. 'wp-content\plugins\groupdocs-viewer\options.php');
+	require(ABSPATH. 'wp-content/plugins/groupdocs-viewer/options.php');
 }
